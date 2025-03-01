623b910aa2f2e3372f5e18f019497be840f3441e
Miroslav Ondra <ondra@faster.cz>
2025-03-01 18:55:54 +0100

    Clean build-tools

--- build-tools/hash_passwd.sh
+++ build-tools/hash_passwd.sh
@@ -11,14 +11,14 @@ configfile="$1"
 
 . "$configfile"
 
-if [ "$CONFIG_SUDO_PASSWD" = "y" ]; then
+if [ "${CONFIG_SUDO_PASSWD:-n}" = "y" ]; then
     PW=$CONFIG_SUDO_PASSWD_TEXT
     if echo "$PW" | grep -qv '^|[[:digit:]]|'; then
         HASHPW=$(openssl passwd -5 -stdin <<< "$PW" | sed 's/\$/|/g')
         sed "s#CONFIG_SUDO_PASSWD_TEXT=.*\$#CONFIG_SUDO_PASSWD_TEXT=\"$HASHPW\"#" -i "$configfile"
     fi
 fi
-if [ "$CONFIG_ROOT_PASSWD" = "y" ]; then
+if [ "${CONFIG_ROOT_PASSWD:-n}" = "y" ]; then
     PW=$CONFIG_ROOT_PASSWD_TEXT
     if echo "$PW" | grep -qv '^|[[:digit:]]|'; then
         HASHPW=$(openssl passwd -5 -stdin <<< "$PW" | sed 's/\$/|/g')
